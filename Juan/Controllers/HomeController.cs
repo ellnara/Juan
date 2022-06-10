937100549e0bcf70b8e83be2b651c4ceaf14f7e3
BP201 <bp201@code.local>
2022-06-10 18:43:51 +0400

    first

---
+++ Juan/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using Juan.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Juan.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            Slide slide = new Slide();
+            return View(slide);
+        }
+    }
+}
---
+++ Juan/Models/Slide.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Juan.Models
+{
+    public class Slide
+    {
+        public int Id { get; set; }
+        public int MyProperty { get; set; }
+        public string Url { get; set; }
+    }
+}
